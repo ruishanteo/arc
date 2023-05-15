537779a78accec87a7f98d7146ddadc95417ce8b
Khant Zaw <khantzawkyaw340@gmail.com>
2023-05-15 15:51:00 +0800

    created planner page

---
+++ src/ModulePlanner/ModuleComponent.js
@@ -0,0 +1,113 @@
+/*
+import { Button, TableCell, TextField } from "@mui/material";
+
+const min = 0;
+const max = 100;
+
+export function ModuleComponent({
+  index,
+  updateText,
+  getText,
+  deleteComponent,
+}) {
+  function onChange(value, dataKey) {
+    updateText(index, dataKey, value);
+  }
+
+  return (
+    <>
+      <TableCell align="center">
+        <Button
+          type="button"
+          onClick={() => deleteComponent(index)}
+          sx={{ backgroundColor: "#fcf4d4", color: "black" }}
+        >
+          —
+        </Button>
+      </TableCell>
+
+      <TableCell align="center">
+        <TextField
+          placeholder={`Assessment `}
+          sx={{
+            width: { sm: 200, md: 250 },
+            "& .MuiInputBase-root": {
+              height: 50,
+            },
+          }}
+          type="text"
+          id="component"
+          value={getText(index, "componentTitle")}
+          onChange={(event) => {
+            onChange(event.target.value, "componentTitle");
+          }}
+        />
+      </TableCell>
+
+      <TableCell align="center">
+        <TextField
+          sx={{
+            width: { sm: 100, md: 150 },
+            "& .MuiInputBase-root": {
+              height: 50,
+            },
+          }}
+          type="number"
+          value={getText(index, "score")}
+          onChange={(event) => {
+            var value = parseInt(event.target.value, 10);
+
+            if (value < min) value = min;
+
+            onChange(value, "score");
+          }}
+          inputProps={{ min, max }}
+        />
+      </TableCell>
+
+      <TableCell align="center">
+        <TextField
+          sx={{
+            width: { sm: 100, md: 150 },
+            "& .MuiInputBase-root": {
+              height: 50,
+            },
+          }}
+          type="number"
+          value={getText(index, "total")}
+          onChange={(event) => {
+            var value = parseInt(event.target.value, 10);
+
+            if (value < min) value = min;
+
+            onChange(value, "total");
+          }}
+          inputProps={{ min, max }}
+        />
+      </TableCell>
+
+      <TableCell align="center">
+        <TextField
+          sx={{
+            width: { sm: 100, md: 150 },
+            "& .MuiInputBase-root": {
+              height: 50,
+            },
+          }}
+          type="number"
+          value={getText(index, "weight")}
+          onChange={(event) => {
+            var value = parseInt(event.target.value, 10);
+
+            if (value > max) value = max;
+            if (value < min) value = min;
+
+            onChange(value, "weight");
+          }}
+          inputProps={{ min, max }}
+        />
+      </TableCell>
+    </>
+  );
+}
+*/
