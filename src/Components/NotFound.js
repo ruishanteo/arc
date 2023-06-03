ef185a2eabb89c8b1ec4c8f3da51bf5098cf9289
ruishanteo <ruishan.teo03@gmail.com>
2023-06-03 10:19:53 +0800

    updated 404 not found page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,17 +1,36 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
+
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
 
 export const NotFound = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      sx={{ mt: 5, height: "90vh" }}
-    >
-      <Grid item xs={3}>
-        <Typography>404 NOT FOUND</Typography>
+    <Box align="center">
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        sx={{
+          mt: 5,
+          height: "50vh",
+          width: "40vw",
+        }}
+      >
+        <Grid item xs={3} />
+        <Grid item xs={3} align="center">
+          <Typography sx={{ fontSize: 70 }}>
+            4 <SentimentDissatisfiedIcon sx={{ fontSize: 50 }} /> 4
+          </Typography>
+
+          <Typography>The page you are looking for can't be found.</Typography>
+          <Button
+            variant="contained"
+            href="/home"
+            sx={{ mt: 2, backgroundColor: "#b7b0f5", color: "white" }}
+          >
+            Go to home
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
