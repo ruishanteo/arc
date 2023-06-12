57cbdaf68d977c0374bc1d7a6864d58848549ba0
ruishanteo <ruishan.teo03@gmail.com>
2023-06-12 14:56:51 +0800

    implemented formik for input

--- src/GradeCalculator/Assessment.js
+++ src/GradeCalculator/Assessment.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 import { store } from "../stores/store";
 import {
@@ -9,6 +11,7 @@ import {
 } from "./GradeStore.js";
 
 import { AssessmentComponent } from "./AssessmentComponent.js";
+import { FormTextField } from "../Components";
 
 import {
   Box,
@@ -64,6 +67,28 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
+const assesmentSchema = Yup.object().shape({
+  components: Yup.array(
+    Yup.object().shape({
+      componentTitle: Yup.string()
+        .required("Please enter a text")
+        .max(15, "Too long"),
+      score: Yup.number()
+        .typeError("Please enter a number")
+        .required("Field is empty")
+        .max(Yup.ref("total"), "Score must be <= total score"),
+      total: Yup.number()
+        .typeError("Please enter a number")
+        .required("Field is empty")
+        .moreThan(0, "Total must be greater than 0"),
+      weight: Yup.number()
+        .typeError("Please enter a number")
+        .required("Field is empty")
+        .moreThan(0, "Weightage must be greater than 0"),
+    })
+  ),
+});
+
 export function Assessment({ assessmentIndex }) {
   const assessment = useSelector(
     (state) => state.calculator.assessments[assessmentIndex]
@@ -79,12 +104,12 @@ export function Assessment({ assessmentIndex }) {
     setExpanded(newExpanded ? panel : false);
   };
 
-  function calculateGrade() {
-    const totalScore = components
-      .filter((t) => t.weight > 0 && t.total > 0 && !t.isDeleted)
+  function calculateGrade(formValues) {
+    const totalScore = formValues.components
+      .filter((t) => t.weight > 0 && t.total > 0)
       .reduce((acc, cur) => acc + (cur.score / cur.total) * cur.weight, 0);
-    const totalWeight = components
-      .filter((t) => t.weight > 0 && t.total > 0 && !t.isDeleted)
+    const totalWeight = formValues.components
+      .filter((t) => t.weight > 0 && t.total > 0)
       .reduce((acc, cur) => acc + cur.weight, 0);
     const currentScore = (totalScore / totalWeight) * 100;
     setCurr(`Current Score: ${currentScore.toFixed(2)}`);
@@ -110,192 +135,234 @@ export function Assessment({ assessmentIndex }) {
         </AccordionSummary>
 
         <AccordionDetails>
-          <Stack align="center">
-            <Box align="left">
-              <Button
-                type="button"
-                onClick={() =>
-                  store.dispatch(deleteAssessment(assessmentIndex))
-                }
-                sx={{ backgroundColor: "#fcf4d4", color: "black" }}
-              >
-                <DeleteOutline />
-              </Button>
-            </Box>
-
-            <Box align="center">
-              <TextField
-                inputProps={{
-                  maxLength: 10,
-                  style: { fontSize: 30, textAlign: "center" },
-                }}
-                placeholder="Module Name"
-                onChange={(e) => {
-                  store.dispatch(
-                    updateAssessment(assessmentIndex, e.target.value)
-                  );
-                }}
-                value={assessment.title}
-                variant="standard"
-                sx={{ width: { lg: "30vw", xs: "80vw" } }}
-              />
-            </Box>
-
-            <Box align="left">
-              <Button
-                variant="contained"
-                onClick={() => store.dispatch(addComponent(assessmentIndex))}
-                startIcon={<Add />}
-                sx={{ mt: 2, mb: 1, width: 130 }}
-                color="neutral"
-              >
-                Component
-              </Button>
-            </Box>
-
-            <Grid
-              container
-              direction="row"
-              sx={{
-                alignItems: "center",
-                backgroundColor: "#cff8df",
-                fontSize: "1.2rem",
-                height: "5vh",
-                mb: 2,
-              }}
-            >
-              <Grid item width="1vw" />
-
-              <Grid item xs={5} sm={5.8} md={6}>
-                <Typography
-                  align="center"
-                  sx={{
-                    width: { lg: "31vw", md: "29vw", sm: "28vw", xs: "25vw" },
-                    fontSize: {
-                      lg: 18,
-                      md: 18,
-                      sm: 15,
-                      xs: 11,
-                    },
-                  }}
-                >
-                  ASSESSMENT
-                </Typography>
-              </Grid>
-
-              <Grid item xs={2} lg={1.8}>
-                <Typography
-                  align="center"
-                  sx={{
-                    width: { lg: "10vw", md: "12vw", sm: "15vw", xs: "18vw" },
-                    fontSize: {
-                      lg: 18,
-                      md: 18,
-                      sm: 15,
-                      xs: 11,
-                    },
-                  }}
-                >
-                  SCORE
-                </Typography>
-              </Grid>
-
-              <Grid item xs={2} lg={1.8}>
-                <Typography
-                  align="center"
-                  sx={{
-                    width: { lg: "10vw", md: "12vw", sm: "15vw", xs: "18vw" },
-                    fontSize: {
-                      lg: 18,
-                      md: 18,
-                      sm: 15,
-                      xs: 11,
-                    },
-                  }}
-                >
-                  TOTAL
-                </Typography>
-              </Grid>
-
-              <Grid item xs={2} md={1.5} lg={1.8}>
-                <Typography
-                  align="center"
-                  sx={{
-                    width: { lg: "10vw", md: "12vw", sm: "15vw", xs: "18vw" },
-                    fontSize: {
-                      lg: 18,
-                      md: 18,
-                      sm: 15,
-                      xs: 11,
-                    },
-                  }}
-                >
-                  WEIGHTAGE
-                </Typography>
-              </Grid>
-            </Grid>
-
-            <Box>
-              {components.map((element, componentIndex) => {
-                return (
-                  <Box key={componentIndex} sx={{ mb: 2 }}>
-                    {!element.isDeleted && (
-                      <AssessmentComponent
-                        componentIndex={componentIndex}
-                        assessmentIndex={assessmentIndex}
-                      />
-                    )}
+          <Formik
+            enableReinitialize={true}
+            initialValues={{ title: "", components: components }}
+            onSubmit={async (values, { setSubmitting }) => {
+              calculateGrade(values);
+            }}
+            validationSchema={assesmentSchema}
+          >
+            {(formikProps) => (
+              <Form>
+                <Stack align="center">
+                  <Box align="left">
+                    <Button
+                      type="button"
+                      onClick={() =>
+                        store.dispatch(deleteAssessment(assessmentIndex))
+                      }
+                      sx={{ backgroundColor: "#fcf4d4", color: "black" }}
+                    >
+                      <DeleteOutline />
+                    </Button>
                   </Box>
-                );
-              })}
-            </Box>
 
-            <Grid container>
-              <Grid item>
-                <Typography sx={{ mt: 4 }}>Desired Score:</Typography>
-              </Grid>
+                  <Box align="center">
+                    <FormTextField
+                      label="title"
+                      type="text"
+                      autoComplete="on"
+                      formikProps={formikProps}
+                      inputProps={{
+                        maxLength: 10,
+                        style: { fontSize: 30, textAlign: "center" },
+                      }}
+                      placeholder="Module Name"
+                      onChange={(e) => {
+                        store.dispatch(
+                          updateAssessment(assessmentIndex, e.target.value)
+                        );
+                      }}
+                      value={assessment.title}
+                      variant="standard"
+                      sx={{ width: { lg: "30vw", xs: "80vw" } }}
+                    />
+                  </Box>
 
-              <Grid item>
-                <TextField
-                  sx={{
-                    mt: 2,
-                    ml: 1,
-                    mr: 2,
-                    width: { sm: 100, md: 150 },
-                    "& .MuiInputBase-root": {
-                      height: 50,
-                    },
-                  }}
-                  type="number"
-                  value={value}
-                  onChange={(event) => {
-                    var value = parseInt(event.target.value, 10);
+                  <Box align="left">
+                    <Button
+                      variant="contained"
+                      onClick={() =>
+                        store.dispatch(addComponent(assessmentIndex))
+                      }
+                      startIcon={<Add />}
+                      sx={{ mt: 2, mb: 1, width: 130 }}
+                      color="neutral"
+                    >
+                      Component
+                    </Button>
+                  </Box>
 
-                    if (value > max) value = max;
-                    if (value < min) value = min;
+                  <Grid
+                    container
+                    direction="row"
+                    sx={{
+                      alignItems: "center",
+                      backgroundColor: "#cff8df",
+                      fontSize: "1.2rem",
+                      height: "5vh",
+                      mb: 2,
+                    }}
+                  >
+                    <Grid item width="1vw" />
 
-                    setValue(value);
-                  }}
-                  inputProps={{ min, max }}
-                />
-              </Grid>
+                    <Grid item xs={5} sm={5.8} md={6}>
+                      <Typography
+                        align="center"
+                        sx={{
+                          width: {
+                            lg: "31vw",
+                            md: "29vw",
+                            sm: "28vw",
+                            xs: "25vw",
+                          },
+                          fontSize: {
+                            lg: 18,
+                            md: 18,
+                            sm: 15,
+                            xs: 11,
+                          },
+                        }}
+                      >
+                        ASSESSMENT
+                      </Typography>
+                    </Grid>
 
-              <Grid item>
-                <Button
-                  type="button"
-                  onClick={calculateGrade}
-                  sx={{ mt: 3, backgroundColor: "#fcf4d4" }}
-                  variant="contained"
-                >
-                  {" "}
-                  Calculate{" "}
-                </Button>
-              </Grid>
-            </Grid>
+                    <Grid item xs={2} lg={1.8}>
+                      <Typography
+                        align="center"
+                        sx={{
+                          width: {
+                            lg: "10vw",
+                            md: "12vw",
+                            sm: "15vw",
+                            xs: "18vw",
+                          },
+                          fontSize: {
+                            lg: 18,
+                            md: 18,
+                            sm: 15,
+                            xs: 11,
+                          },
+                        }}
+                      >
+                        SCORE
+                      </Typography>
+                    </Grid>
 
-            {curr && <Typography sx={{ mt: 2 }}>{curr}</Typography>}
-            {result && <Typography>{result}</Typography>}
-          </Stack>
+                    <Grid item xs={2} lg={1.8}>
+                      <Typography
+                        align="center"
+                        sx={{
+                          width: {
+                            lg: "10vw",
+                            md: "12vw",
+                            sm: "15vw",
+                            xs: "18vw",
+                          },
+                          fontSize: {
+                            lg: 18,
+                            md: 18,
+                            sm: 15,
+                            xs: 11,
+                          },
+                        }}
+                      >
+                        TOTAL
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={2} md={1.5} lg={1.8}>
+                      <Typography
+                        align="center"
+                        sx={{
+                          width: {
+                            lg: "10vw",
+                            md: "12vw",
+                            sm: "15vw",
+                            xs: "18vw",
+                          },
+                          fontSize: {
+                            lg: 18,
+                            md: 18,
+                            sm: 15,
+                            xs: 11,
+                          },
+                        }}
+                      >
+                        WEIGHTAGE
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Box>
+                    {components.map((element, componentIndex) => {
+                      return (
+                        <Box key={componentIndex} sx={{ mb: 2 }}>
+                          {!element.isDeleted && (
+                            <AssessmentComponent
+                              componentIndex={componentIndex}
+                              assessmentIndex={assessmentIndex}
+                              formikProps={formikProps}
+                            />
+                          )}
+                        </Box>
+                      );
+                    })}
+                  </Box>
+
+                  <Grid container>
+                    <Grid item>
+                      <Typography sx={{ mt: 4 }}>Desired Score:</Typography>
+                    </Grid>
+
+                    <Grid item>
+                      <TextField
+                        sx={{
+                          mt: 2,
+                          ml: 1,
+                          mr: 2,
+                          width: { sm: 100, md: 150 },
+                          "& .MuiInputBase-root": {
+                            height: 50,
+                          },
+                        }}
+                        type="number"
+                        value={value}
+                        onChange={(event) => {
+                          var value = parseInt(event.target.value, 10);
+
+                          if (value > max) value = max;
+                          if (value < min) value = min;
+
+                          setValue(value);
+                        }}
+                        inputProps={{ min, max }}
+                      />
+                    </Grid>
+
+                    <Grid item>
+                      <Button
+                        type="submit"
+                        // onClick={calculateGrade}
+                        sx={{ mt: 3, backgroundColor: "#fcf4d4" }}
+                        variant="contained"
+                      >
+                        {" "}
+                        Calculate{" "}
+                      </Button>
+                    </Grid>
+                  </Grid>
+
+                  <Box align="left">
+                    {curr && <Typography sx={{ mt: 2 }}>{curr}</Typography>}
+                    {result && <Typography>{result}</Typography>}
+                  </Box>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </AccordionDetails>
       </Accordion>
     </div>
--- src/GradeCalculator/AssessmentComponent.js
+++ src/GradeCalculator/AssessmentComponent.js
@@ -1,15 +1,29 @@
+import React from "react";
 import { useSelector } from "react-redux";
 
 import { store } from "../stores/store";
 import { deleteComponent, updateComponent } from "./GradeStore";
 
-import { Grid, IconButton, TextField } from "@mui/material";
+import { FormTextField } from "../Components";
+
+import { Grid, IconButton, Typography } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 
 const min = 0;
 const max = 100;
 
-export function AssessmentComponent({ assessmentIndex, componentIndex }) {
+const DISPLAY_NAMES = {
+  componentTitle: "ASSESSMENT",
+  score: "SCORE",
+  total: "TOTAL",
+  weight: "WEIGHTAGE",
+};
+
+export function AssessmentComponent({
+  assessmentIndex,
+  componentIndex,
+  formikProps,
+}) {
   const component = useSelector(
     (state) =>
       state.calculator.assessments[assessmentIndex].components[componentIndex]
@@ -23,109 +37,147 @@ export function AssessmentComponent({ assessmentIndex, componentIndex }) {
 
   return (
     <Grid container maxWidth="lg" align="center">
-      <Grid item width="1vw" sx={{ display: { lg: "flex", xs: "none" } }} />
+      <Grid container maxWidth="lg" align="center">
+        <Grid item width="1vw" sx={{ display: { lg: "flex", xs: "none" } }} />
 
-      <Grid
-        item
-        xs={5.5}
-        sm={5.8}
-        md={6}
-        sx={{
-          align: "center",
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <IconButton
-          type="button"
-          onClick={() =>
-            store.dispatch(deleteComponent(assessmentIndex, componentIndex))
-          }
+        <Grid
+          item
+          xs={5.5}
+          sm={5.8}
+          md={6}
           sx={{
-            backgroundColor: "#fcf4d4",
-            borderRadius: 1,
-            color: "black",
-            height: "5vh",
-            width: "2vw",
-            mr: 1,
+            align: "center",
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "row",
           }}
         >
-          <ClearIcon />
-        </IconButton>
+          <IconButton
+            type="button"
+            onClick={() =>
+              store.dispatch(deleteComponent(assessmentIndex, componentIndex))
+            }
+            sx={{
+              backgroundColor: "#fcf4d4",
+              borderRadius: 1,
+              color: "black",
+              height: "5vh",
+              width: "2vw",
+              mr: 1,
+            }}
+          >
+            <ClearIcon />
+          </IconButton>
 
-        <TextField
-          fullWidth
-          placeholder="Assessment"
-          type="text"
-          id="component"
-          value={component.componentTitle}
-          onChange={(event) => {
-            onChange(event.target.value, "componentTitle");
-          }}
-          inputProps={{
-            min,
-            max,
-            style: { fontSize: "0.75rem" },
-          }}
-        />
+          <FormTextField
+            label={`components[${componentIndex}].componentTitle`}
+            type="text"
+            formikProps={formikProps}
+            hideError={true}
+            fullWidth
+            placeholder="Assessment"
+            value={component.componentTitle}
+            onChange={(event) => {
+              onChange(event.target.value, "componentTitle");
+            }}
+            inputProps={{
+              min,
+              max,
+              style: { fontSize: "0.75rem" },
+              maxLength: 15,
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={2} lg={1.8}>
+          <FormTextField
+            label={`components[${componentIndex}].score`}
+            type="number"
+            formikProps={formikProps}
+            hideError={true}
+            value={component.score}
+            onChange={(event) => {
+              var value = parseInt(event.target.value, 10);
+              if (value < min) value = min;
+              onChange(value, "score");
+            }}
+            inputProps={{
+              min,
+              max,
+              style: { fontSize: "0.75rem" },
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={2} lg={1.8}>
+          <FormTextField
+            label={`components[${componentIndex}].total`}
+            type="number"
+            formikProps={formikProps}
+            hideError={true}
+            value={component.total}
+            onChange={(event) => {
+              var value = parseInt(event.target.value, 10);
+
+              if (value < min) value = min;
+
+              onChange(value, "total");
+            }}
+            inputProps={{
+              min,
+              max,
+              style: { fontSize: "0.75rem" },
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={2} lg={1.8}>
+          <FormTextField
+            label={`components[${componentIndex}].weight`}
+            type="number"
+            formikProps={formikProps}
+            hideError={true}
+            value={component.weight}
+            onChange={(event) => {
+              var value = parseInt(event.target.value, 10);
+
+              if (value > max) value = max;
+              if (value < min) value = min;
+
+              onChange(value, "weight");
+            }}
+            inputProps={{
+              min,
+              max,
+              style: { fontSize: "0.75rem" },
+            }}
+          />
+        </Grid>
       </Grid>
+      {!(formikProps.isValid ?? formikProps.isValid) &&
+        formikProps.errors &&
+        formikProps.errors.components &&
+        formikProps.errors.components[componentIndex] && (
+          <Grid container maxWidth="lg" align="center">
+            <Grid>
+              <Typography align="left" variant="body2">
+                The following errors occurred while trying to calculate:
+              </Typography>
 
-      <Grid item xs={2} lg={1.8}>
-        <TextField
-          type="number"
-          value={component.score}
-          onChange={(event) => {
-            var value = parseInt(event.target.value, 10);
-            if (value < min) value = min;
-            onChange(value, "score");
-          }}
-          inputProps={{
-            min,
-            max,
-            style: { fontSize: "0.75rem" },
-          }}
-        />
-      </Grid>
-
-      <Grid item xs={2} lg={1.8}>
-        <TextField
-          type="number"
-          value={component.total}
-          onChange={(event) => {
-            var value = parseInt(event.target.value, 10);
-
-            if (value < min) value = min;
-
-            onChange(value, "total");
-          }}
-          inputProps={{
-            min,
-            max,
-            style: { fontSize: "0.75rem" },
-          }}
-        />
-      </Grid>
-
-      <Grid item xs={2} lg={1.8}>
-        <TextField
-          type="number"
-          value={component.weight}
-          onChange={(event) => {
-            var value = parseInt(event.target.value, 10);
-
-            if (value > max) value = max;
-            if (value < min) value = min;
-
-            onChange(value, "weight");
-          }}
-          inputProps={{
-            min,
-            max,
-            style: { fontSize: "0.75rem" },
-          }}
-        />
-      </Grid>
+              {Object.keys(formikProps.errors.components[componentIndex]).map(
+                (key, index) => {
+                  const value =
+                    formikProps.errors.components[componentIndex][key];
+                  return (
+                    <Typography key={index} align="left" variant="body2">
+                      {`\n\u2022 ${DISPLAY_NAMES[key]}: ${value}`}
+                    </Typography>
+                  );
+                }
+              )}
+            </Grid>
+          </Grid>
+        )}
     </Grid>
   );
 }
