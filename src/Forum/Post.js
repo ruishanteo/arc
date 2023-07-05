62add9e0d086db68ba1f45356fe2a89e3407221b
ruishanteo <ruishan.teo03@gmail.com>
2023-07-05 08:41:10 +0800

    improved ui layout for post and comment view

--- src/Forum/Post.js
+++ src/Forum/Post.js
@@ -71,64 +71,64 @@ export function Post() {
 
   return (
     <Container maxWidth="lg" align="center">
-      <Box align="center" maxWidth="65vw">
-        <Box align="center" sx={{ mt: 5, mb: 2 }}>
-          <Box align="left">
-            <Button
-              sx={{
-                backgroundColor: "#b7b0f5",
-                color: "white",
-                maxHeight: "5vh",
-              }}
-              variant="contained"
-              onClick={() => navigate("/forum")}
-            >
-              <ArrowBackIos /> Back
-            </Button>
-          </Box>
-
-          <Box
-            display="flex"
-            flexDirection="row"
-            sx={{ mt: 5, mb: 2 }}
-            align="left"
+      <Box align="center" sx={{ mt: 5, mb: 2 }}>
+        <Box align="left">
+          <Button
+            sx={{
+              backgroundColor: "#b7b0f5",
+              color: "white",
+              maxHeight: "5vh",
+            }}
+            variant="contained"
+            onClick={() => navigate("/forum")}
           >
-            <Avatar
-              src={users[post.author.userId]?.photoURL}
-              sx={{
-                width: 100,
-                height: 100,
-                cursor: "pointer",
-                mb: 2,
-                mr: 2,
-              }}
-            />
+            <ArrowBackIos /> Back
+          </Button>
+        </Box>
 
-            {editMode ? (
-              <Formik
-                initialValues={{ title: post.title, post: post.post }}
-                validationSchema={Yup.object().shape({
-                  title: Yup.string().required("Required"),
-                  post: Yup.string().required("Required"),
-                })}
-                onSubmit={async (values) => {
-                  await store
-                    .dispatch(
-                      editPost(
-                        {
-                          ...values,
-                          datetime: Timestamp.fromDate(new Date()),
-                        },
-                        id
-                      )
+        <Box
+          display="flex"
+          flexDirection="row"
+          sx={{ mt: 5, mb: 2 }}
+          align="left"
+        >
+          <Avatar
+            src={users[post.author.userId]?.photoURL}
+            sx={{
+              width: 100,
+              height: 100,
+              cursor: "pointer",
+              mb: 2,
+              mr: 2,
+            }}
+          />
+
+          {editMode ? (
+            <Formik
+              initialValues={{ title: post.title, post: post.post }}
+              validationSchema={Yup.object().shape({
+                title: Yup.string().required("Required"),
+                post: Yup.string().required("Required"),
+              })}
+              onSubmit={async (values) => {
+                await store
+                  .dispatch(
+                    editPost(
+                      {
+                        ...values,
+                        datetime: Timestamp.fromDate(new Date()),
+                      },
+                      id
                     )
-                    .finally(() => onUpdate());
-                  setEditMode(false);
-                }}
-              >
-                {(formikProps) => (
-                  <Form id="post-form">
-                    <Box display="flex" flexDirection="column" width="60vw">
+                  )
+                  .finally(() => onUpdate());
+                setEditMode(false);
+              }}
+            >
+              {(formikProps) => (
+                <Form id="post-form">
+                  <Grid container direction="column">
+                    <Grid item width="67vw">
                       <FormTextField
                         label="title"
                         type="text"
@@ -136,8 +136,11 @@ export function Post() {
                         formikProps={formikProps}
                         inputProps={{ maxLength: 100 }}
                         placeholder="Enter title here"
+                        fullWidth
                       />
+                    </Grid>
 
+                    <Grid item>
                       <FormTextField
                         label="post"
                         type="text"
@@ -145,11 +148,26 @@ export function Post() {
                         formikProps={formikProps}
                         placeholder="Enter text here"
                         sx={{ mt: 2 }}
-                        rows={5}
+                        rows={10}
                         multiline
+                        fullWidth
                       />
+                    </Grid>
 
-                      <Box display="flex" flexDirection="row">
+                    <Grid container>
+                      <Grid item>
+                        <IconButton
+                          sx={{
+                            backgroundColor: "#fcf4d4",
+                            mr: 1,
+                            borderRadius: 1,
+                          }}
+                          onClick={() => setEditMode(false)}
+                        >
+                          <Close />
+                        </IconButton>
+                      </Grid>
+                      <Grid item>
                         <IconButton
                           type="submit"
                           disabled={
@@ -157,92 +175,92 @@ export function Post() {
                             formikProps.isSubmitting ||
                             loading
                           }
+                          sx={{
+                            backgroundColor: "#ffe0f7",
+                            mr: 1,
+                            borderRadius: 1,
+                          }}
                         >
                           <Done />
                         </IconButton>
-
-                        <IconButton onClick={() => setEditMode(false)}>
-                          <Close />
-                        </IconButton>
-                      </Box>
-                    </Box>
-                  </Form>
-                )}
-              </Formik>
-            ) : (
-              <Grid container direction="row">
-                <Grid item xs={10}>
-                  <Typography variant="h4" sx={{ wordBreak: "break-word" }}>
-                    {post.title}
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    sx={{ mt: 2, wordBreak: "break-word" }}
-                  >
-                    {post.post}
-                  </Typography>
-                </Grid>
-
-                {user.uid === post.author.userId && (
-                  <Grid item>
-                    <IconButton
-                      onClick={() => setOpen(true)}
-                      aria-label="delete"
-                      variant="contained"
-                      sx={{
-                        backgroundColor: "#fcf4d4",
-                        mr: 1,
-                        borderRadius: 1,
-                      }}
-                    >
-                      <DeleteOutline />
-                    </IconButton>
-                    <IconButton
-                      aria-label="edit"
-                      variant="contained"
-                      sx={{ backgroundColor: "#ffe0f7", borderRadius: 1 }}
-                      onClick={() => setEditMode(true)}
-                    >
-                      <ModeEditOutline />
-                    </IconButton>
+                      </Grid>
+                    </Grid>
                   </Grid>
-                )}
+                </Form>
+              )}
+            </Formik>
+          ) : (
+            <Grid container direction="row">
+              <Grid item xs={10}>
+                <Typography variant="h4" sx={{ wordBreak: "break-word" }}>
+                  {post.title}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ mt: 2, wordBreak: "break-word" }}
+                >
+                  {post.post}
+                </Typography>
               </Grid>
-            )}
-          </Box>
-          <Box align="right" sx={{ mr: 4 }}>
-            <Typography variant="subtitle1" sx={{ wordBreak: "break-word" }}>
-              {" "}
-              - {users[post.author.userId]?.name}
-            </Typography>
-            <Typography variant="caption">{post.datetime}</Typography>
-          </Box>
+
+              {user.uid === post.author.userId && (
+                <Grid item>
+                  <IconButton
+                    onClick={() => setOpen(true)}
+                    aria-label="delete"
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "#fcf4d4",
+                      mr: 1,
+                      borderRadius: 1,
+                    }}
+                  >
+                    <DeleteOutline />
+                  </IconButton>
+                  <IconButton
+                    aria-label="edit"
+                    variant="contained"
+                    sx={{ backgroundColor: "#ffe0f7", borderRadius: 1 }}
+                    onClick={() => setEditMode(true)}
+                  >
+                    <ModeEditOutline />
+                  </IconButton>
+                </Grid>
+              )}
+            </Grid>
+          )}
         </Box>
-
-        <Dialog
-          open={open}
-          onClose={() => setOpen(false)}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Doing so will delete this post. Click confirm to proceed.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setOpen(false)} sx={{ color: "#b7b0f5" }}>
-              Cancel
-            </Button>
-            <Button onClick={handleDeletePost} autoFocus variant="contained">
-              Confirm
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        <Comment postId={id} posterId={post.author.userId} />
+        <Box align="right" sx={{ mr: 4 }}>
+          <Typography variant="subtitle1" sx={{ wordBreak: "break-word" }}>
+            - {users[post.author.userId]?.name}
+          </Typography>
+          <Typography variant="caption">{post.datetime}</Typography>
+        </Box>
       </Box>
+
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Doing so will delete this post. Click confirm to proceed.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} sx={{ color: "#b7b0f5" }}>
+            Cancel
+          </Button>
+          <Button onClick={handleDeletePost} autoFocus variant="contained">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Comment postId={id} posterId={post.author.userId} />
     </Container>
   );
 }
