4474ba5fc72c53b9aab25db710e2c784834b6c65
Khant Zaw <85423095+keizeee@users.noreply.github.com>
2023-07-09 22:06:10 +0800

    added rate limit

--- src/FeedbackForm/FeedbackStore.js
+++ src/FeedbackForm/FeedbackStore.js
@@ -10,6 +10,10 @@ import { addNotification } from "../Notifications";
 import { db } from "../UserAuth/Firebase";
 import { handleErrorMessage } from "../UserAuth/FirebaseHooks";
 
+const RATE_LIMIT_DURATION = 10000;
+
+let lastFeedbackTimestamp = 0;
+
 const handleApiCall = async (func) => {
   return func.then((res) => res).catch(handleErrorMessage);
 };
@@ -31,18 +35,41 @@ const feedbackSlice = createSlice({
 
 export function createFeedback(feedback) {
   return async (dispatch, getState) => {
+    const currentTimestamp = Date.now();
+
+    // Check if the rate limit duration has elapsed since the last feedback request
+    if (currentTimestamp - lastFeedbackTimestamp < RATE_LIMIT_DURATION) {
+      dispatch(
+        addNotification({
+          message: "Please wait before submitting another feedback.",
+          variant: "error",
+        })
+      );
+      return;
+    }
+
+    // Update the timestamp of the last feedback request
+    lastFeedbackTimestamp = currentTimestamp;
+
     return await handleApiCall(
       addDoc(collection(db, "feedbacks"), {
-        ...feedback
-      }).then((res) => {
-        dispatch(
-          addNotification({
-            message: "Your feedback has been submitted.",
-            variant: "success",
-          })
-        );
-        return res;
+        ...feedback,
       })
+        .then((res) => {
+          dispatch(
+            addNotification({
+              message: "Your feedback has been submitted.",
+              variant: "success",
+            })
+          );
+          return res;
+        })
+        .finally(() => {
+          // Reset the timestamp after the rate limit duration has elapsed
+          setTimeout(() => {
+            lastFeedbackTimestamp = 0;
+          }, RATE_LIMIT_DURATION);
+        })
     );
   };
 }
