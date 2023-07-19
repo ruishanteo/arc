0cd67b2aa665cfd76434e2f41ebf88f6efd96e97
Khant Zaw <khantzawkyaw340@gmail.com>
2023-07-19 17:11:35 +0800

    fixed bug for prog autocomplete

--- src/ModulePlanner/ModuleComponent.js
+++ src/ModulePlanner/ModuleComponent.js
@@ -49,7 +49,7 @@ export function ModuleComponent({
   const handleFilterOptions = (options, state) => {
     const { inputValue } = state;
   
-    if (degrees.length > 0 && module.category.title === "UE" && degrees[0] !== null) {
+    if (degrees.length > 0 && module.category.title === "UE" && degrees[0] !== null && typeof degrees[0] !== 'undefined') {
       const deg = degrees[0].title;
       const moduleCodes = progMods[deg].flatMap((item) => item.moduleCode);
   
@@ -77,7 +77,7 @@ export function ModuleComponent({
   const handleFilterOptionsCateg = (options, state) => {
     const { inputValue } = state;
   
-    if (degrees.length > 0 && degrees[0] !== null) {
+    if (degrees.length > 0 && degrees[0] !== null && typeof degrees[0] !== 'undefined') {
       const deg = degrees[0].title;
       const moduleCodes = progMods[deg].flatMap((item) => item.moduleCode);
   
